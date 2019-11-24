41b349eeb248095284bfb88964ca3675d9f44c1a
allenkis1990 <allenkis1990@163.com>
2019-11-24 17:05:47 +0800

    12121212

--- src/project/portal/utils/mod-loader.js
+++ src/project/portal/utils/mod-loader.js
@@ -1,6 +1,6 @@
 
 import {lwh_demo2,lwh_demo2Components,lwh_demo2Routers,lwh_demo2Stores} from '@mods/@lwh-demo2/index'
-//模块多了后一个个合并进来
+//模块多了后一个个合并进来啊
 export const Routers = [...lwh_demo2Routers]
 export const Stores = {...lwh_demo2Stores}
 export const Components = {...lwh_demo2Components}
